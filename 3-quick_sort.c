f7b513932a85d6a242e40cacdacdf2a7024a534f
Danielyilma <yilmad752@gmail.com>
2023-12-15 09:07:14 +0300

    sorting algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,37 +1,74 @@
 #include "sort.h"
 
+/**
+ * quick_sort - implementation of quick sort with recursion
+ *
+ * @array: array to be sorted
+ * @size: the size of the array
+ *
+ * Return: nothing
+*/
+
 void quick_sort(int *array, size_t size)
 {
-    split(array, 0, size, size);
+	split(array, 0, size, size);
 }
+
+/**
+ * split - sorts by splitting left and right
+ * with respect to pivot
+ *
+ * @array: the array to be sorted
+ * @start: the start of left or the right with respect to pivot
+ * @end: end of the splitted array
+ * @size: size of the array
+ *
+ * Return: nothing
+*/
 
 void split(int *array, int start, int end, int size)
 {
-    int i = start, pivot = end - 1;
+	int i = start, pivot = end - 1;
+	int temp_piv = start;
 
-    if (start == end)
-    return;
+	if (start >= pivot)
+	return;
 
-    for (; i < end; i++)
-    {
-        if (array[i] > array[pivot] && pivot > i)
-        {
-            swap(&array[i], &array[pivot]);
-            print_array(array, size);
-        }
-        else if (array[i] < array[pivot] && pivot < i)
-        {
-            swap(&array[i], &array[pivot]);
-            print_array(array, size);
-        }
-    }
-    split(array, start, pivot, size);
-    split(array, pivot + 1, end, size);
+	for (; i < end - 1; i++)
+	{
+		if (array[i] <= array[pivot] && temp_piv != i)
+		{
+			swap(&array[i], &array[temp_piv]);
+			temp_piv++;
+			print_array(array, size);
+		}
+		else if (array[i] <= array[pivot] && temp_piv == i)
+		temp_piv++;
+	}
+
+	if (temp_piv != pivot)
+	{
+		swap(&array[temp_piv], &array[pivot]);
+		print_array(array, size);
+		pivot = temp_piv;
+	}
+
+	split(array, start, pivot, size);
+	split(array, pivot + 1, end, size);
 }
+
+/**
+ * swap - swaps two elements content
+ *
+ * @a: the first element
+ * @b: the second element
+ *
+ * Return: nothing
+*/
 
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
