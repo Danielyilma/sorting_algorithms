bf201728a18c04aa0acc1e4e3a5e9b05d6b293fd
Danielyilma <yilmad752@gmail.com>
2023-12-19 20:58:33 +0300

    sorting algorithms

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -14,6 +14,8 @@ void cocktail_sort_list(listint_t **list)
 	listint_t *iter;
 	int size = size_of_list(*list), i = 0, j = 0;
 
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+
 	for (i = 0; i < size - 1; i++)
 	{
 		iter = (*list)->next;
